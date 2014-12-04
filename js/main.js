ab26997b3f8375cc513b87bfb43697ca27b2f191
Ipalibo <ipalibowhyte@gmail.com>
2014-12-04 04:17:20 +0000

    added dynamic color changes, added sub colours for red and green

--- js/main.js
+++ js/main.js
@@ -2,13 +2,18 @@ var plateDisplayed = false;
 var isTesting = true;
 var mode = 'Swift';
 
-$(document).ready(function(){
+var colors = {
+	'red' : ['D31900', 'F3210C', 'CE0A31', 'FA023C', 'D2505A', '990100', 'B90504', 'E02130'],
+	'green' : ['A8DBA8', '3B8686', 'CBE86B', '3FB8AF', 'BEF202', '519548', 'B3CC57', '379F7A'],
+}
 
+$(document).ready(function(){
 	intialize();
 
 	$('.controlContainer ul li').click(function(){
 		$('.controlContainer ul li').removeClass();
-		$(this).addClass('selected');
+		$('.controlContainer ul li').removeClass('defaultBackground').css("background-color", "");
+		$(this).addClass('selected defaultBackground animated bounceIn');
 		var lang_type = $(this).attr('language-type');
 		$('textarea').attr('placeholder', lang_type + ' Code');
 		switchLanguage(lang_type);
@@ -40,51 +45,80 @@ $(document).ready(function(){
 		var colorType = $(this).attr('color-type');
 		$('#select-body-label').text(colorType);
 		closeColorPlate();
+		switch(colorType){
+			case 'red':
+				fillColorPlates(colors.red);
+				break;
+			case 'green':
+				fillColorPlates(colors.green);
+				break;
+		}
+		
+	});
+
+	$('.colorCircleContainer').children().click(function(){
+		$('.hexin').val($(this).attr('color'));
+		convertTo($(this).attr('color'));
+		$('.rightSide').addClass('animated flash');
+		$('.rightSide').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+			$('.rightSide').removeClass('animated flash');
+		});
 	});
 
 });
 
+function fillColorPlates(colors){
+	$('.colorCircleContainer').children().each(function(index){
+		$(this).attr('color', colors[index]);
+		$(this).css('background-color', '#'+colors[index]);
+	});
+}
+
 function validate(hex){
-	if(hex.length == 6){
+	hex = hex.toLowerCase();
+
+	if(hex.length == 6 && hex.match(/^[0-9a-z]+$/)){
 		return true
 	}
-
 	return false;
 }
 
 function convertTo(hex){
 
-	if(hex.length == 6 || isTesting){
+	if(validate(hex)){
 
 	var red_val = hex.slice(0, 2);
 	var green_val = hex.slice(2, 4);
 	var blue_val = hex.slice(4, 6);
 
 	red_val = parseInt(red_val, 16);
-	green_val = parseInt(blue_val, 16);
-	blue_val = parseInt(green_val, 16);
+	green_val = parseInt(green_val, 16);
+	blue_val = parseInt(blue_val, 16);
 
 	console.log(red_val+ ',' +green_val+ ',' +blue_val);
 
 	handleResult(red_val, green_val, blue_val);
-	}else{
-		alert('invalid');
+
+	$('.defaultBackground').css('background-color', '#'+hex);
+	$('.defaultColor').css('color', '#'+hex);
 	}
 	
 }
 
 function handleResult(red, green, blue){
+	// round to 1 dp
 	switch(mode){
 		case 'Swift':
-			$('textarea').text('UIColor(red: '+red+', green: '+green+', blue: '+blue+', alpha: 1.0)');
+			$('textarea').val('UIColor(red: '+(Math.round( red/255 * 1000 ) / 1000)+', green: '+(Math.round( green/255 * 1000 ) / 1000)+', blue: '+(Math.round( blue/255 * 1000 ) / 1000)+', alpha: 1.0)');
 			break;
 		case 'Objective-C':
-			$('textarea').text('[UIColor colorWithRed:'+red+' green:'+green+'blue:'+blue+' alpha:1.0f]');
+			$('textarea').val('[UIColor colorWithRed:'+(Math.round( red/255 * 1000 ) / 1000)+' green:'+(Math.round( green/255 * 1000 ) / 1000)+' blue:'+(Math.round( blue/255 * 1000 ) / 1000)+' alpha:1.0]');
 			break;
 		case 'Rgb':
-			$('textarea').text('rgb('+red+','+green+','+blue+')');
+			$('textarea').val('rgb('+red+','+green+','+blue+')');
 			break;
 	}
+
 	activate($('button#copy'));
 }
 
