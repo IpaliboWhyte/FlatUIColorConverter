6c4e00f660ee3395303ceb6394f8f2e025c3c10f
Ipalibo <ipalibowhyte@gmail.com>
2014-12-04 23:58:20 +0000

    added zeroclip

--- js/main.js
+++ js/main.js
@@ -24,7 +24,6 @@ client.on( "ready", function( readyEvent ) {
   client.on( "aftercopy", function( event ) {
     // `this` === `client`
     // `event.target` === the element that was clicked
-    alert("Copied text to clipboard: " + event.data["text/plain"] );
   } );
 
 } );
